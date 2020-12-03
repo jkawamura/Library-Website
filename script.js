a64af482253381fb44aea755af2500dc7743e585
Joseph Kawamura <joseph.kawamura@gmail.com>
2020-12-03 00:12:56 -0800

    added signup page

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 function login(){
   document.getElementById("loginF").style.display="block";
 }
+
+function signUp(){
+  document.getElementById("signupF").style.display="block";
+}
+
+function closeForm(){
+  document.getElementById("loginF").style.display="none";
+}
+
+function confirmPassword(){
+  if(document.getElementById("cpsw").value == document.getElementById("signup-psw").value){
+    document.getElementById("confirm").innerHTML = "Passwords Match";
+    document.getElementById("confirm").style.color = "green";
+  } else{
+    document.getElementById("confirm").innerHTML = "Passwords Don't Match";
+    document.getElementById("confirm").style.color = "red";
+  }
+}
+
