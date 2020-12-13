b5726aa622b673874707381329b1a79a4e188542
abbiehurwitz <34584513+abbiehurwitz@users.noreply.github.com>
2020-12-13 15:39:29 -0500

    started the php for returning a book

---
+++ returnBook.php
@@ -0,0 +1,29 @@
+<?php
+$title = $_POST['title'];
+$author = $_POST['author'];
+
+$servername = 'localhost';
+$username = 'gtstudent';
+$password = '';
+$dbname = 'test';
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+          die("Connection failed: " . $conn->connect_error);
+              }
+        $query = "select * from jk_books where author = '" . $author . "' and title = 'title';";
+        $result = $conn->query($query);
+        if($result){
+                $row = $result->fetch_assoc();
+                $check = "available";
+                if($row['status'] == $check){
+                echo "<p>this book is not currently being borrowed</p>";
+                }
+                else{
+
+}                                                                                                                                                                                                                                                                                           }else{
+                echo "<p>this book does not exist in the library</p>";
+        }
+
+
+?>
